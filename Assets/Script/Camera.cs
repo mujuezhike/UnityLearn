7f808f017352e0cc505b7cf89ac9cc59653f7152
mujuezhike <37540006@qq.com>
2019-04-16 09:18:19 +0800

    'unitystart'

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.Rotate(new Vector3(0.2f,0.6f,0.2f), 0.05f);
+        this.transform.Rotate(new Vector3(0, -1, 0), 0.3f);
+    }
+}
---
+++ Assets/Script/Tank.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject cube;
+	void Start () {
+        int m = 10;
+        while( m > 0)
+        { 
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    GameObject go = GameObject.Instantiate<GameObject>(cube);
+                    go.transform.position = new Vector3(5+ 1.1f * (j + 1)+(10-m)*0.55f, (10-m)*1.1f , 1.1f * (i + 1) + (10 - m) * 0.55f);
+                }
+            }
+            m--;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.Rotate(new Vector3(0,1,0), 3);
+        this.transform.Rotate(new Vector3(0,0,1), 0.05f);
+    }
+}
